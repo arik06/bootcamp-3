23d77456dc97052aaa5b232a27688a666d900a45
arik06 <111409356+arik06@users.noreply.github.com>
2022-11-27 01:13:44 -0600

    3er commit

--- script.js
+++ script.js
@@ -23,8 +23,25 @@ document.querySelector(".icon").src =
   document.querySelector(".temp").innerText = temp + "°C";
   document.querySelector(".humidity").innerText = humidity + "%";
   document.querySelector(".wind").innerText = "Wind speed" + speed + "km/h";
+},
+search: function(){
+    this.fetchWeather(document.querySelector(".search-bar").value);
 }
-
-    
 };
 
+document.querySelector(".search button").addEventListener("click" , function(){
+weather.search();
+
+
+});
+
+document.querySelector(".search-bar").addEventListener("keyup" , function(event){ 
+
+if (event.key == "Enter") {
+weather.search();
+}
+
+
+});
+
+weather.fetchWeather("Mexico");
